37c7f2b721ea0fb94af232ef036f5b76300f8581
Ryder Smith <rsmith@kbsi.com>
2019-12-13 11:35:32 -0600

    feat(Button) Add support for fabs

--- src/components/button/Button.svelte
+++ src/components/button/Button.svelte
@@ -1,16 +1,18 @@
 <script>
 import { normalizeColor } from '../../utils/color'
 
-export let dense = false
+export let fab = false
 export let tile = false
+export let dense = false
 export let color = 'blue'
 export let textColor = 'white'
 export let click = () => {}
 
 const classes = [
   'r--button',
-  dense && 'r--button--dense',
+  fab && 'r--button--fab',
   tile && 'r--button--tile',
+  dense && 'r--button--dense',
   `bg-${normalizeColor(color)}`,
   `hover:bg-${normalizeColor(color, 200)}`,
   `text-${normalizeColor(textColor)}`,
@@ -20,12 +22,29 @@ const classes = [
 
 <style global>
 .r--button {
-  @apply font-bold py-2 px-4 rounded;
+  @apply inline-flex items-center justify-center;
+  @apply py-2 px-4;
+  @apply rounded;
+  @apply font-bold;
+
   &.r--button--dense {
     @apply py-1 px-2;
   }
+
   &.r--button--tile {
     @apply rounded-none;
+  }
+
+  &.r--button--fab {
+    @apply rounded-full h-12 w-12;
+
+    &:not(.r--button--dense) {
+      @apply h-12 w-12;
+    }
+
+    &.r--button--dense {
+      @apply h-8 w-8;
+    }
   }
 }
 </style>
