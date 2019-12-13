b7b659f6998791a68188c43c526643ea03618d4c
Ryder Smith <rsmith@kbsi.com>
2019-12-13 10:54:08 -0600

    feat(RButton) Created r--button-styles

--- src/components/button/Button.svelte
+++ src/components/button/Button.svelte
@@ -1,5 +1,5 @@
 <script>
-import { normalizeColor } from '../utils/color'
+import { normalizeColor } from '../../utils/color'
 
 export let dense = false
 export let tile = false
@@ -8,16 +8,27 @@ export let textColor = 'white'
 export let click = () => {}
 
 const classes = [
+  'r--button',
+  dense && 'r--button--dense',
+  tile && 'r--button--tile',
   `bg-${normalizeColor(color)}`,
   `hover:bg-${normalizeColor(color, 200)}`,
   `text-${normalizeColor(textColor)}`,
-  'font-bold',
-  dense ? 'py-1' : 'py-2',
-  dense ? 'px-2' : 'px-4',
-  !tile && 'rounded',
 ].filter(x => x).join(' ')
 
 </script>
+
+<style global>
+.r--button {
+  @apply font-bold py-2 px-4 rounded;
+  &.r--button--dense {
+    @apply py-1 px-2;
+  }
+  &.r--button--tile {
+    @apply rounded-none;
+  }
+}
+</style>
 
 <button class="{classes}" on:click={click}>
   <slot>Button</slot>
