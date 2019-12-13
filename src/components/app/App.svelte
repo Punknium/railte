89d8412066db6608f1075c44657ab7b22fcd0de6
Ryder Smith <rsmith@kbsi.com>
2019-12-13 11:36:30 -0600

    chore(App) Move tailwind to app level style

---
+++ src/components/app/App.svelte
@@ -0,0 +1,23 @@
+<style global>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+::-webkit-scrollbar-track {
+  background: #F5F5F5;
+}
+::-webkit-scrollbar-thumb {
+  background: #CCC;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #AAA;
+}
+::-webkit-scrollbar-thumb:active {
+  background: #888;
+  box-shadow: inset 1px 1px 2px rgba(0,0,0,0.3);
+  -webkit-box-shadow: inset 1px 1px 2px rgba(0,0,0,0.3);
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import App from './app/App.svelte'
 import Button from './button/Button.svelte'
 import Card from './card/Card.svelte'
 import Container from './container/Container.svelte'
